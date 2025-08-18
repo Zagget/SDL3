25a24759a55c5a9baba25b91ae2e811bc39f98a1
Jonathan Romskaug <jonathan.romskaug@gmail.com>
2025-08-18 15:52:18 +0200

    Inital commit

---
+++ main.h
@@ -0,0 +1,36 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_image.h>
+
+int main(int argc, char** argv) {
+	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD);
+	SDL_Window* win = SDL_CreateWindow("SDL3 Demo", 800, 600, 0);
+	SDL_Renderer* ren = SDL_CreateRenderer(win, NULL);
+
+	bool running = true;
+	SDL_Event ev;
+
+	while (running) {
+		//Event handling
+		while (SDL_PollEvent(&ev)) {
+			if (ev.key.key == SDLK_ESCAPE) {
+				running = false;
+			}
+		}
+
+		//clear last frame
+		SDL_RenderClear(ren);
+
+		// draw calls
+
+		//make rect to render sprite into
+		SDL_FRect* dst = new SDL_FRect{ 100,100,64,64 };
+		//SDL_RenderTexture(ren, sprite, NULL, dst);
+
+		SDL_RenderPresent(ren);
+	}
+
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(win);
+	SDL_Quit();
+	return 0;
+}
