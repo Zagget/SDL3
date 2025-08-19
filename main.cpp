8299779c1ccf455542774f7a958503def50435ac
Jonathan Romskaug <jonathan.romskaug@gmail.com>
2025-08-19 11:31:48 +0200

    +Added bounds

--- main.cpp
+++ main.cpp
@@ -4,18 +4,23 @@
 
 int main(int argc, char** argv) {
 
+    int width = 1200;
+    int height = 1000;
+
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD);
-    SDL_Window* win = SDL_CreateWindow("SDL3 Demo", 800, 600, 0);
+    SDL_Window* win = SDL_CreateWindow("SDL3 Demo", width, height, 0);
     SDL_Renderer* ren = SDL_CreateRenderer(win, NULL);
+
+    //background color
+    SDL_SetRenderDrawColor(ren, 2, 75, 120, 255);
 
     SDL_Texture* sprite = IMG_LoadTexture(ren, "confusedman.png");
     if (!sprite) {
         std::cerr << "SDL Init Failed: " << SDL_GetError() << '\n';
-        return 0;
     }
 
-    float startingXPos = 800 / 2 - 50;
-    float startingYPos = 600 / 2 - 50;
+    float startingXPos = width / 2 - 50;
+    float startingYPos = height / 2 - 50;
 
     float xPos = startingXPos;
     float yPos = startingYPos;
@@ -26,7 +31,6 @@ int main(int argc, char** argv) {
     bool running = true;
     SDL_Event ev;
     while (running) {
-        //Event handling
         while (SDL_PollEvent(&ev)) {
             if (ev.key.key == SDLK_ESCAPE) {
                 running = false;
@@ -56,6 +60,11 @@ int main(int argc, char** argv) {
 
         xPos += xMove * moveScale;
         yPos += yMove * moveScale;
+
+        if (xPos < 0) xPos = 0;
+        if (yPos < 0) yPos = 0;
+        if (xPos > width - 100) xPos = width - 100;
+        if (yPos > height - 100) yPos = height - 100; 
 
         //clear last frame
         SDL_RenderClear(ren);
