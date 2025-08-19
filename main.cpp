bd838d33124a11a135e3aeb780fb6c6e21acfa68
Jonathan Romskaug <jonathan.romskaug@gmail.com>
2025-08-19 11:22:42 +0200

    +Move sprite with wasd

--- main.cpp
+++ main.cpp
@@ -14,6 +14,15 @@ int main(int argc, char** argv) {
         return 0;
     }
 
+    float startingXPos = 800 / 2 - 50;
+    float startingYPos = 600 / 2 - 50;
+
+    float xPos = startingXPos;
+    float yPos = startingYPos;
+
+    float xMove, yMove;
+    float moveScale = 0.05;
+
     bool running = true;
     SDL_Event ev;
     while (running) {
@@ -22,7 +31,31 @@ int main(int argc, char** argv) {
             if (ev.key.key == SDLK_ESCAPE) {
                 running = false;
             }
+            if (ev.type == SDL_EVENT_QUIT) {
+                running = false;
+            }
         }
+
+        // Polling
+        const bool* keys = SDL_GetKeyboardState(NULL);
+        SDL_PumpEvents();
+
+
+        //move sprite
+        xMove = 0;
+        yMove = 0;
+
+        if (keys[SDL_SCANCODE_W])
+            yMove = -1;
+        if (keys[SDL_SCANCODE_A])
+            xMove = -1;
+        if (keys[SDL_SCANCODE_S])
+            yMove = 1;
+        if (keys[SDL_SCANCODE_D])
+            xMove = 1;
+
+        xPos += xMove * moveScale;
+        yPos += yMove * moveScale;
 
         //clear last frame
         SDL_RenderClear(ren);
@@ -30,7 +63,7 @@ int main(int argc, char** argv) {
         // draw calls
 
         //make rect to render sprite into
-        SDL_FRect* dst = new SDL_FRect{ 100,100,64,64 };
+        SDL_FRect* dst = new SDL_FRect{ xPos,yPos,100,100};
         SDL_RenderTexture(ren, sprite, NULL, dst);
 
         SDL_RenderPresent(ren);
